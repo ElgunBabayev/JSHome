4d6b188674f0d4d2f7ae8ee3505b0b794c55b53a
Matrisa <90101246+Matrisa@users.noreply.github.com>
2021-11-27 01:13:15 +0400

    Add files via upload

---
+++ homeworkJS.js
@@ -0,0 +1,47 @@
+/*
+while (true) {
+    let season = prompt("enter your number.");
+  
+    switch (true) {
+      case season < 1 || season > 4:
+        alert("error");
+        break;
+      case (season == 1):
+        alert("winter");
+        break;
+      case (season == 2):
+        alert("spring");
+        break;
+      case (season == 3):
+        alert("summer");
+        break;
+      case (season == 4):
+        alert("autumn");
+        break;
+     
+    }
+  }
+
+  (tam bolen)
+let num = prompt("eded daxil edin");
+
+for (i = 1; i <= num; i++) {
+  if (num % i == 0) {
+    console.log(i);
+  }
+}
+(faktorial)
+let num=prompt("ededi daxil edin");
+result=1;
+for (i= 1; i<=num; i++) {
+    result*=i;
+    console.log(result);
+}
+*/
+let credit =prompt("krediti daxil edin")
+let month = prompt("nece ayliq")
+
+
+  
+
+
